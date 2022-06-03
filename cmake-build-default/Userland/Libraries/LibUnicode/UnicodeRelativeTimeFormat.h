a96e4eb05edc57009fe8bd093f93a43e01408731
LEE <15267030696@163.com>
2022-06-03 15:05:37 +0800

    1.delete cmake-build-default/;

--- cmake-build-default/Userland/Libraries/LibUnicode/UnicodeRelativeTimeFormat.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include <LibUnicode/Forward.h>
-
-namespace Unicode {
-
-}
--- cmake-build-default/Userland/Libraries/LibWeb/CSS/QuirksModeStyleSheetSource.cpp
+++
@@ -1,8 +0,0 @@
-namespace Web::CSS {
-extern const char quirks_mode_stylesheet_source[];
-const char quirks_mode_stylesheet_source[] = "\
-table {\
-    text-align: left;\
-}\
-";
-}
